aaba8f0538ed2041cb3ce6be440a118666540963
Peter Schueler <peter@orangemirrorentertainment.com>
2018-05-20 23:27:31 +0200

    Added Basic Teleportation Mechanic

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		var destPrefs = PlayerPrefs.GetString("destination");
+		if (destPrefs != "") {
+			PlayerPrefs.SetString("destination", "");
+			player.transform.position = GameObject.Find(destPrefs).transform.position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour {
+	public bool local;
+	public System.String destination;
+	public System.String scene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnTriggerEnter(Collider col) {
+		if (local) {
+			var dest = GameObject.Find(destination);
+			col.gameObject.transform.position = dest.transform.position;
+		} else {
+			PlayerPrefs.SetString("destination", destination);
+			SceneManager.LoadScene(scene);
+		}
+	}
+}
