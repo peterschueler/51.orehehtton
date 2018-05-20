964b1b94c5fe4e03fad2c01731e6617396a35b95
Peter Schueler <peter@orangemirrorentertainment.com>
2018-05-21 00:50:24 +0200

    Added Light Prefabs; Textures

---
+++ Assets/FloorLightController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorLightController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var rand = Random.Range (-9, 14);
+		foreach (Light l in gameObject.GetComponentsInChildren<Light>()) {
+			if (rand > -3 && rand < 2) {
+				l.enabled = false;
+			} else {
+				l.enabled = true;
+			}
+		}
+	}
+}
