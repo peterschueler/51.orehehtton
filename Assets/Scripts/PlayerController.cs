6e0ee44a75a394da5a9b56b2bed856ee13a26dab
Peter Schueler <peter@orangemirrorentertainment.com>
2018-05-20 21:32:11 +0200

    Added Basic Character Controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float x_movement;
+	public float z_movement;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * x_movement;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * z_movement;
+		
+		transform.Rotate(0,x,0);
+		transform.Translate(0,0,z);
+	}
+}
