e05a479764df46b20bf9154aeef07541e854c8da
Peter Schueler <peter@orangemirrorentertainment.com>
2018-05-24 01:20:58 +0200

    Added New Scene, Models

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+	public GameObject[] tapes;
+	public GameObject[] glitches;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (var r in tapes) {
+			r.transform.Rotate(0,0,20*Time.deltaTime);
+		}
+		var rand = Random.Range (-9, 14);
+		foreach(var g in glitches) {
+			if (rand > -3 && rand < 2) {
+				g.SetActive(false);
+			} else {
+				g.SetActive(true);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/RenderTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderTrigger : MonoBehaviour {
+	public GameObject[] renderObjects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	
+	void OnTriggerEnter(Collider col) {
+		foreach(var obj in renderObjects) {
+			if (obj.GetComponent<Renderer>().enabled) {
+				obj.GetComponent<Renderer>().enabled = false;
+			} else {
+				obj.GetComponent<Renderer>().enabled = true;
+			}
+		}
+	}
+}
