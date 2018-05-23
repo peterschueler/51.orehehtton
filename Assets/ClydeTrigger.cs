1d782f80af8e4527eaa2d8072b49ef1bf799e8be
Peter Schueler <peter@orangemirrorentertainment.com>
2018-05-23 23:05:04 +0200

    Added Rudimentary Runthrough For First Part

---
+++ Assets/ClydeTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class ClydeTrigger : MonoBehaviour {
+	public GameObject clyde;
+	public GameObject[] lights;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider col) {
+		clyde.SetActive(true);
+		foreach (var l in lights) {
+			l.SetActive(true);
+		}
+	}
+}
--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Trigger : MonoBehaviour {
 	public GameObject caption;
 	public ParticleSystem effect;
 	public GameObject moodLight;
 	public float delay;
+	public float sceneDelay;
+	public string scene;
 
 	// Use this for initialization
 	void Start () {
@@ -25,7 +28,13 @@ public class Trigger : MonoBehaviour {
 		caption.transform.position = new Vector3(caption.transform.position.x, caption.transform.position.y, caption.transform.position.z - 0.2f);
 	}
 	
+	IEnumerator LoadSceneAfterTime(float time) {
+		yield return new WaitForSeconds(time);
+		SceneManager.LoadScene(scene);
+	}
+	
 	void OnTriggerEnter(Collider col) {
 		StartCoroutine(ExecuteAfterTime(delay));
+		StartCoroutine(LoadSceneAfterTime(sceneDelay));
 	}
 }
