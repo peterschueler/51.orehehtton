2bcc87d21f811ded20e0e6b22ae11eb7a830b62a
Peter Schueler <peter@orangemirrorentertainment.com>
2018-05-24 22:30:43 +0200

    Added Glitch Resetter; Tweaked Player Settings

---
+++ Assets/Scripts/ResetGlitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetGlitch : MonoBehaviour {
+	public GameObject[] renderObjects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider col) {
+		foreach(var obj in renderObjects) {
+			obj.GetComponent<Renderer>().enabled = true;
+		}
+	}
+}
